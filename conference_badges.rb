22b98cb9936dfb6bbe1323e98d62a22026f39e8c
Kaitlin Ryan <kaitlinaryan1@gmail.com>
2018-02-05 17:33:22 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,6 @@
 # Write your code here.
 def badge_maker(name)
-  name = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-  return "Hello, my name is #{name}."
+  return "Hello, my name is Arel."
 end
 
 def batch_badge_creator(names)
