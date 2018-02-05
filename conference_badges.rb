21aab8da9b3f29a56aabd3d3cc7620cbe84a8394
Kaitlin Ryan <kaitlinaryan1@gmail.com>
2018-02-05 17:40:00 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,7 +6,5 @@ end
 def batch_badge_creator(names)
 speakers = ["Hello, my name is Edsger.", "Hello, my name is Ada.", "Hello, my name is Charles.", "Hello, my name is Alan.", "Hello, my name is Grace.", "Hello, my
 name is Linus.", "Hello, my name is Matz."]
-speakers.each do |speaker|
-  puts "Hello, my name is #{speaker}"
-end
+
 end
