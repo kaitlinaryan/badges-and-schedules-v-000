e9b6942ddcc0148f60e8e9e06d0cd457e749f42c
Kaitlin Ryan <kaitlinaryan1@gmail.com>
2018-02-05 17:56:28 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,11 +1,14 @@
-# Write your code here.
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(names)
-names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-names.each do |name|
-puts "Hello, my name is #{name}."
+def batch_badge_creator(speakers)
+badges = []
+speakers.each do |name|
+  badges.push(badge_maker(name))
 end
+badges
 end
