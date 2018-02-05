bb5b2cc3fa9a2252a837008dedb8de0d20743b6b
Kaitlin Ryan <kaitlinaryan1@gmail.com>
2018-02-05 17:27:01 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,8 @@
 # Write your code here.
+def badge_maker
+  puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+end
