d458991f854cbf40835f45b5fc52ccc8a003972c
Kaitlin Ryan <kaitlinaryan1@gmail.com>
2018-02-05 18:07:12 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,19 +6,4 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(speakers)
-badges = []
-speakers.each do |name|
-  badges.push(badge_maker(name))
-end
-badges
-end
-
-def assign_rooms(speakers)
-  rooms = []
-  room_number = 1
-  speakers.each do |name|
-    rooms.push("Hello, #{name}! You'll be assigned to room #{room_number}!")
-    room_number += 1
-  end
-  rooms
 end
